93e0945ea83b6961ee76dfb194849c12e069cc72
Daniel <xch13779509255@gmail.com>
2026-07-29 11:00:03 +0800

    修改serial

--- 9-2串口发送+接受/Hardware/Serial.c
+++ 9-2串口发送+接受/Hardware/Serial.c
@@ -2,16 +2,26 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+uint8_t Serial_RxData;
+uint8_t Serial_RxFlag;
+
 void Serial_Init()
 {
     // 开启时钟
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
 
-    // GPIO初始化
+    // GPIO初始化 TX
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP; //复用推挽输出
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+        // GPIO初始化 RX
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; //上拉输入
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 
@@ -22,8 +32,17 @@ void Serial_Init()
     USART_Initstructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None; // 硬件流控
     USART_Initstructure.USART_StopBits = USART_StopBits_1;                          // 停止位
     USART_Initstructure.USART_Parity = USART_Parity_No;                             // 奇偶校验
-    USART_Initstructure.USART_Mode = USART_Mode_Tx;                                 // 模式
+    USART_Initstructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;                                 // 模式 输入和输出
     USART_Init(USART1, &USART_Initstructure);
+
+    //中断设置
+    NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;		
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;
+    NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+
+    NVIC_Init(&NVIC_InitStructure);
 
     // USART使能
     USART_Cmd(USART1, ENABLE);
@@ -92,4 +111,30 @@ void Serial_printf(char *format, ...)
     vsprintf(String, format, arg);
     va_end(arg);
     Serial_SendString(String);
+}
+
+
+//获取串口接收标识位
+uint8_t Serial_GetRxFlag(void){
+    if(Serial_RxFlag==1){
+
+        Serial_RxFlag=0;
+        return 1;
+    }
+    return 0;
+}
+
+//获取串口接收的数据
+uint8_t Serial_GetRxData(){
+    return Serial_RxData;
+}
+
+
+//USART中断函数
+void USART1_IRQHandler(){
+    if(USART_GetITStatus(USART1,USART_IT_RXNE)==SET){
+        Serial_RxData=USART_ReceiveData(USART1);
+        Serial_RxFlag=1;
+        USART_ClearITPendingBit(USART1,USART_IT_RXNE);
+    }
 }
